588913bddf82ac55c490ded0833e052de116918a
Daen Rebel <daenrebel035@gmail.com>
2020-10-21 11:33:38 +0200

    Update to include compilation through babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,30 @@
-import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-    {
-      file: pkg.browser,
-      format: 'iife',
-      name: 'checkEleven',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-  ],
-  plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-    terser(),
-  ],
-};
+const extensions = ['.js', '.ts'];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+      }),
+    ],
+  },
+];
