605d384b8ee08656a514a0afb26118b9e1463e79
Daen Rebel <daenrebel035@gmail.com>
2020-11-02 18:00:44 +0100

    Update eleven check to accept 8 digit numbers

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  * @param numberToParse Input number
  * @returns {Array<number>}
  */
-export function arrayFromNumber(numberToParse: number): Array<number> {
+export function arrayFromNumber (numberToParse: number): Array<number> {
   // Create a string for the numberToParse argument
   const numberString = numberToParse.toString();
 
@@ -15,20 +15,19 @@ export function arrayFromNumber(numberToParse: number): Array<number> {
 }
 
 /**
- * Dutch person number check
- * Check built to match specs found on http://nl.wikipedia.org/wiki/Burgerservicenummer
+ * Eleven check
+ * Check built to match specs found on https://nl.wikipedia.org/wiki/Elfproef
  *
- * @param {number} personNumber
+ * @param {number} checkNumber
  * @returns {boolean} Is the number valid
  */
-export function check(personNumber: number): boolean {
-  // Check if the number is between 9999999 and 999999999
-  if (personNumber <= 9999999 || personNumber > 999999999) {
-    return false;
-  }
+export function check (checkNumber: number): boolean {
+  const numberArray = arrayFromNumber(checkNumber);
 
-  // Sum is not 0 and sum is dividable by 0 without restant
-  // return sum !== 0 && (sum % 11 === 0);
-  return arrayFromNumber(personNumber)
-    .reduce((p, c, i) => p + (c * (i !== 8 ? 9 - i : -1)), 0) % 11 === 0;
+  const { length } = numberArray;
+
+  const sum = numberArray
+    .reduce((p, c, i) => p + (c * (i !== length - 1 ? length - i : -1)), 0);
+
+  return sum % 11 === 0 && sum > 0;
 }
